76722d268f5bac15012fe36a9d0af35f97aa4248
Alexander Kuldoshin <Nomorinus@gmail.com>
2025-06-10 10:16:58 +0000

    Ok, let's implement this XML in 6 steps 1 by 1 and i will tell you if we

---
+++ src/locales/en/portPriceFinderForm.ts
@@ -0,0 +1,5 @@
+import type { Translations } from '@/contexts/LocalizationContext';
+
+export const portPriceFinderFormEnTranslations: Partial<Translations> = {
+  // English translations for PortPriceFinderForm will be moved here
+};
---
+++ src/locales/ru/portPriceFinderForm.ts
@@ -0,0 +1,5 @@
+import type { Translations } from '@/contexts/LocalizationContext';
+
+export const portPriceFinderFormRuTranslations: Partial<Translations> = {
+  // Russian translations for PortPriceFinderForm will be moved here
+};
