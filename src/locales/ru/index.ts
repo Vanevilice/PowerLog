370cb7f710c5b92361d7a2e9c5ec253895e173f3
Alexander Kuldoshin <Nomorinus@gmail.com>
2025-06-16 07:37:53 +0000

    Yes, lets proceed

--- src/locales/ru/index.ts
+++ src/locales/ru/index.ts
@@ -2,10 +2,12 @@
 import type { Translations } from '@/contexts/LocalizationContext';
 import { portPriceFinderFormRuTranslations } from './portPriceFinderForm';
 import { dashboardPageRuTranslations } from './dashboardPage'; // Import dashboard translations
+import { bestPricesPageRuTranslations } from './bestPricesPage'; // Import best prices translations
 
 // This object will merge all partial Russian translation files.
 export const partialRuTranslations: Partial<Translations> = {
   ...portPriceFinderFormRuTranslations,
   ...dashboardPageRuTranslations, // Merge dashboard translations
+  ...bestPricesPageRuTranslations, // Merge best prices translations
   // ... other future partial Russian translations can be merged here
 };
